95aaa946e22fb073527584527b960afb3824364f
Lgp007-IES <lgueper960@g.educaand.es>
2026-05-21 10:03:28 +0200

    Inicio proyecto

---
+++ src/main/java/ChatCraft/ReservaHoteles/ReservaHotelesApplication.java
@@ -0,0 +1,13 @@
+package ChatCraft.ReservaHoteles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaHotelesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaHotelesApplication.class, args);
+	}
+
+}
